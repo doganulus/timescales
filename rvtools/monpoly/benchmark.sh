6ac9bf07aa3403ffe88aaf8188c9afd26f5f179b
Dogan Ulus <dogan.ulus@boun.edu.tr>
2023-08-13 16:29:15 +0300

    Update benchmark containers

--- rvtools/monpoly/benchmark.sh
+++ rvtools/monpoly/benchmark.sh
@@ -2,64 +2,64 @@
 hyperfine \
     --warmup 2 \
     --runs 10 \
-    --export-json "/timescales/results/monpoly.discrete.1M.$(date +%s).json" \
+    --export-json "results/monpoly.discrete.1M.$(date +%s).json" \
     --command-name AbsentAQ10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentAQ10.mtl -log /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentAQ10.mtl -log fullsuite/AbsentAQ/Discrete/1M/AbsentAQ10.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentAQ100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentAQ100.mtl -log /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentAQ100.mtl -log fullsuite/AbsentAQ/Discrete/1M/AbsentAQ100.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentAQ1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentAQ1000.mtl -log /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentAQ1000.mtl -log fullsuite/AbsentAQ/Discrete/1M/AbsentAQ1000.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentBQR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentBQR10.mtl -log /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentBQR10.mtl -log fullsuite/AbsentBQR/Discrete/1M/AbsentBQR10.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentBQR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentBQR100.mtl -log /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentBQR100.mtl -log fullsuite/AbsentBQR/Discrete/1M/AbsentBQR100.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentBQR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentBQR1000.mtl -log /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentBQR1000.mtl -log fullsuite/AbsentBQR/Discrete/1M/AbsentBQR1000.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentBR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentBR10.mtl -log /timescales/fullsuite/AbsentBR/Discrete/1M/AbsentBR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentBR10.mtl -log fullsuite/AbsentBR/Discrete/1M/AbsentBR10.monpoly.log > /dev/null 2>&1" \
     --command-name AbsentBR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AbsentBR100.mtl -log /timescales/fullsuite/AbsentBR/Discrete/1M/AbsentBR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AbsentBR100.mtl -log fullsuite/AbsentBR/Discrete/1M/AbsentBR100.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBR1000.mtl -log /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBR1000.mtl -log fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysAQ10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysAQ10.mtl -log /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysAQ10.mtl -log fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ10.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysAQ100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysAQ100.mtl -log /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysAQ100.mtl -log fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ100.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysAQ1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysAQ1000.mtl -log /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysAQ1000.mtl -log fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ1000.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBQR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBQR10.mtl -log /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBQR10.mtl -log fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR10.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBQR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBQR100.mtl -log /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBQR100.mtl -log fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR100.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBQR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBQR1000.mtl -log /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBQR1000.mtl -log fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR1000.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBR10.mtl -log /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBR10.mtl -log fullsuite/AlwaysBR/Discrete/1M/AlwaysBR10.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBR100.mtl -log /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBR100.mtl -log fullsuite/AlwaysBR/Discrete/1M/AlwaysBR100.monpoly.log > /dev/null 2>&1" \
     --command-name AlwaysBR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/AlwaysBR1000.mtl -log /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/AlwaysBR1000.mtl -log fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.monpoly.log > /dev/null 2>&1" \
     --command-name RecurBQR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurBQR10.mtl -log /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurBQR10.mtl -log fullsuite/RecurBQR/Discrete/1M/RecurBQR10.monpoly.log > /dev/null 2>&1" \
     --command-name RecurBQR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurBQR100.mtl -log /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurBQR100.mtl -log fullsuite/RecurBQR/Discrete/1M/RecurBQR100.monpoly.log > /dev/null 2>&1" \
     --command-name RecurBQR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurBQR1000.mtl -log /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurBQR1000.mtl -log fullsuite/RecurBQR/Discrete/1M/RecurBQR1000.monpoly.log > /dev/null 2>&1" \
     --command-name RecurGLB10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurGLB10.mtl -log /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurGLB10.mtl -log fullsuite/RecurGLB/Discrete/1M/RecurGLB10.monpoly.log > /dev/null 2>&1" \
     --command-name RecurGLB100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurGLB100.mtl -log /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurGLB100.mtl -log fullsuite/RecurGLB/Discrete/1M/RecurGLB100.monpoly.log > /dev/null 2>&1" \
     --command-name RecurGLB1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RecurGLB1000.mtl -log /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RecurGLB1000.mtl -log fullsuite/RecurGLB/Discrete/1M/RecurGLB1000.monpoly.log > /dev/null 2>&1" \
     --command-name RespondBQR10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondBQR10.mtl -log /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondBQR10.mtl -log fullsuite/RespondBQR/Discrete/1M/RespondBQR10.monpoly.log > /dev/null 2>&1" \
     --command-name RespondBQR100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondBQR100.mtl -log /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondBQR100.mtl -log fullsuite/RespondBQR/Discrete/1M/RespondBQR100.monpoly.log > /dev/null 2>&1" \
     --command-name RespondBQR1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondBQR1000.mtl -log /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondBQR1000.mtl -log fullsuite/RespondBQR/Discrete/1M/RespondBQR1000.monpoly.log > /dev/null 2>&1" \
     --command-name RespondGLB10 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondGLB10.mtl -log /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB10.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondGLB10.mtl -log fullsuite/RespondGLB/Discrete/1M/RespondGLB10.monpoly.log > /dev/null 2>&1" \
     --command-name RespondGLB100 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondGLB100.mtl -log /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB100.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondGLB100.mtl -log fullsuite/RespondGLB/Discrete/1M/RespondGLB100.monpoly.log > /dev/null 2>&1" \
     --command-name RespondGLB1000 \
-        "monpoly -sig /timescales/rvtools/monpoly/resources/timescales.sig -formula /timescales/rvtools/monpoly/resources/RespondGLB1000.mtl -log /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB1000.monpoly.log > /dev/null 2>&1" \
+        "monpoly -sig rvtools/monpoly/resources/timescales.sig -formula rvtools/monpoly/resources/RespondGLB1000.mtl -log fullsuite/RespondGLB/Discrete/1M/RespondGLB1000.monpoly.log > /dev/null 2>&1" \
--- rvtools/montre/benchmark.sh
+++ rvtools/montre/benchmark.sh
@@ -2,67 +2,67 @@
 hyperfine \
     --warmup 3 \
     --runs 25 \
-    --export-json "/timescales/results/montre.discrete.1M.$(date +%s).json" \
+    --export-json "results/montre.discrete.1M.$(date +%s).json" \
     --command-name AbsentAQ10 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,9))' -f /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ10.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,9))' -f fullsuite/AbsentAQ/Discrete/1M/AbsentAQ10.txt >> /dev/null 2>&1" \
     --command-name AbsentAQ100 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,99))' -f /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ100.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,99))' -f fullsuite/AbsentAQ/Discrete/1M/AbsentAQ100.txt >> /dev/null 2>&1" \
     --command-name AbsentAQ1000 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,999))' -f /timescales/fullsuite/AbsentAQ/Discrete/1M/AbsentAQ1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,999))' -f fullsuite/AbsentAQ/Discrete/1M/AbsentAQ1000.txt >> /dev/null 2>&1" \
     --command-name AbsentBR10 \
-        "montre -b '(<:p|(<:!p)%(0,9));<:r:>' -f /timescales/fullsuite/AbsentBR/Discrete/1M/AbsentBR10.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,9));<:r:>' -f fullsuite/AbsentBR/Discrete/1M/AbsentBR10.txt >> /dev/null 2>&1" \
     --command-name AbsentBR100 \
-        "montre -b '(<:p|(<:!p)%(0,99));<:r:>' -f /timescales/fullsuite/AbsentBR/Discrete/1M/AbsentBR100.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,99));<:r:>' -f fullsuite/AbsentBR/Discrete/1M/AbsentBR100.txt >> /dev/null 2>&1" \
     --command-name AbsentBR1000 \
-        "montre -b '(<:p|(<:!p)%(0,999));<:r:>' -f /timescales/fullsuite/AbsentBR/Discrete/1M/AbsentBR1000.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,999));<:r:>' -f fullsuite/AbsentBR/Discrete/1M/AbsentBR1000.txt >> /dev/null 2>&1" \
     --command-name AbsentBQR10 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,2)|(!p&&!q&&!r)%(11,1000000));<:r:>' -f /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR10.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,2)|(!p&&!q&&!r)%(11,1000000));<:r:>' -f fullsuite/AbsentBQR/Discrete/1M/AbsentBQR10.txt >> /dev/null 2>&1" \
     --command-name AbsentBQR100 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,29)|(!p&&!q&&!r)%(101,1000000));<:r:>' -f /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR100.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,29)|(!p&&!q&&!r)%(101,1000000));<:r:>' -f fullsuite/AbsentBQR/Discrete/1M/AbsentBQR100.txt >> /dev/null 2>&1" \
     --command-name AbsentBQR1000 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,299)|(!p&&!q&&!r)%(1001,1000000));<:r:>' -f /timescales/fullsuite/AbsentBQR/Discrete/1M/AbsentBQR1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,299)|(!p&&!q&&!r)%(1001,1000000));<:r:>' -f fullsuite/AbsentBQR/Discrete/1M/AbsentBQR1000.txt >> /dev/null 2>&1" \
     --command-name AlwaysAQ10 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,9))' -f /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ10.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,9))' -f fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ10.txt >> /dev/null 2>&1" \
     --command-name AlwaysAQ100 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,99))' -f /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ100.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,99))' -f fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ100.txt >> /dev/null 2>&1" \
     --command-name AlwaysAQ1000 \
-        "montre -b '<:q:>;(p:>|(!p:>)%(0,999))' -f /timescales/fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p:>|(!p:>)%(0,999))' -f fullsuite/AlwaysAQ/Discrete/1M/AlwaysAQ1000.txt >> /dev/null 2>&1" \
     --command-name AlwaysBR10 \
-        "montre -b '(<:p|(<:!p)%(0,9));<:r:>' -f /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR10.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,9));<:r:>' -f fullsuite/AlwaysBR/Discrete/1M/AlwaysBR10.txt >> /dev/null 2>&1" \
     --command-name AlwaysBR100 \
-        "montre -b '(<:p|(<:!p)%(0,99));<:r:>' -f /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR100.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,99));<:r:>' -f fullsuite/AlwaysBR/Discrete/1M/AlwaysBR100.txt >> /dev/null 2>&1" \
     --command-name AlwaysBR1000 \
-        "montre -b '(<:p|(<:!p)%(0,999));<:r:>' -f /timescales/fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p|(<:!p)%(0,999));<:r:>' -f fullsuite/AlwaysBR/Discrete/1M/AlwaysBR1000.txt >> /dev/null 2>&1" \
     --command-name AlwaysBQR10 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,2)|(!p&&!q&&!r)%(11,1000000));<:r:>' -f /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR10.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,2)|(!p&&!q&&!r)%(11,1000000));<:r:>' -f fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR10.txt >> /dev/null 2>&1" \
     --command-name AlwaysBQR100 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,29)|(!p&&!q&&!r)%(101,1000000));<:r:>' -f /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR100.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,29)|(!p&&!q&&!r)%(101,1000000));<:r:>' -f fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR100.txt >> /dev/null 2>&1" \
     --command-name AlwaysBQR1000 \
-        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,299)|(!p&&!q&&!r)%(1001,1000000));<:r:>' -f /timescales/fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:q:>;(p|(!p&&!q&&!r)%(0,299)|(!p&&!q&&!r)%(1001,1000000));<:r:>' -f fullsuite/AlwaysBQR/Discrete/1M/AlwaysBQR1000.txt >> /dev/null 2>&1" \
     --command-name RecurGLB10 \
-        "montre -b '<:p;(!p:>)%(11,1000000)' -f /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB10.txt >> /dev/null 2>&1" \
+        "montre -b '<:p;(!p:>)%(11,1000000)' -f fullsuite/RecurGLB/Discrete/1M/RecurGLB10.txt >> /dev/null 2>&1" \
     --command-name RecurGLB100 \
-        "montre -b '<:p;(!p:>)%(101,1000000)' -f /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB100.txt >> /dev/null 2>&1" \
+        "montre -b '<:p;(!p:>)%(101,1000000)' -f fullsuite/RecurGLB/Discrete/1M/RecurGLB100.txt >> /dev/null 2>&1" \
     --command-name RecurGLB1000 \
-        "montre -b '<:p;(!p:>)%(1001,1000000)' -f /timescales/fullsuite/RecurGLB/Discrete/1M/RecurGLB1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:p;(!p:>)%(1001,1000000)' -f fullsuite/RecurGLB/Discrete/1M/RecurGLB1000.txt >> /dev/null 2>&1" \
     --command-name RecurBQR10 \
-        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(11,1000000);<:r:>' -f /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR10.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(11,1000000);<:r:>' -f fullsuite/RecurBQR/Discrete/1M/RecurBQR10.txt >> /dev/null 2>&1" \
     --command-name RecurBQR100 \
-        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(101,1000000);<:r:>' -f /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR100.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(101,1000000);<:r:>' -f fullsuite/RecurBQR/Discrete/1M/RecurBQR100.txt >> /dev/null 2>&1" \
     --command-name RecurBQR1000 \
-        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(1001,1000000);<:r:>' -f /timescales/fullsuite/RecurBQR/Discrete/1M/RecurBQR1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;<:p:>;((!p&&!r):>)%(1001,1000000);<:r:>' -f fullsuite/RecurBQR/Discrete/1M/RecurBQR1000.txt >> /dev/null 2>&1" \
     --command-name RespondGLB10 \
-        "montre -b '(<:p:>;(!s)%(0,2);<:s:>)|(<:p:>;(!s:>)%(11,1000000))' -f /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB10.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p:>;(!s)%(0,2);<:s:>)|(<:p:>;(!s:>)%(11,1000000))' -f fullsuite/RespondGLB/Discrete/1M/RespondGLB10.txt >> /dev/null 2>&1" \
     --command-name RespondGLB100 \
-        "montre -b '(<:p:>;(!s)%(0,29);<:s:>)|(<:p:>;(!s:>)%(101,1000000))' -f /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB100.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p:>;(!s)%(0,29);<:s:>)|(<:p:>;(!s:>)%(101,1000000))' -f fullsuite/RespondGLB/Discrete/1M/RespondGLB100.txt >> /dev/null 2>&1" \
     --command-name RespondGLB1000 \
-        "montre -b '(<:p:>;(!s)%(0,299);<:s:>)|(<:p:>;(!s:>)%(1001,1000000))' -f /timescales/fullsuite/RespondGLB/Discrete/1M/RespondGLB1000.txt >> /dev/null 2>&1" \
+        "montre -b '(<:p:>;(!s)%(0,299);<:s:>)|(<:p:>;(!s:>)%(1001,1000000))' -f fullsuite/RespondGLB/Discrete/1M/RespondGLB1000.txt >> /dev/null 2>&1" \
     --command-name RespondBQR10 \
-        "montre -b '<:!q;((<:p:>;(!s)%(0,2);<:s:>)|(<:p:>;(!s:>)%(11,1000000)));!r:>' -f /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR10.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;((<:p:>;(!s)%(0,2);<:s:>)|(<:p:>;(!s:>)%(11,1000000)));!r:>' -f fullsuite/RespondBQR/Discrete/1M/RespondBQR10.txt >> /dev/null 2>&1" \
     --command-name RespondBQR100 \
-        "montre -b '<:!q;((<:p:>;(!s)%(0,29);<:s:>)|(<:p:>;(!s:>)%(101,1000000)));!r:>' -f /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR100.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;((<:p:>;(!s)%(0,29);<:s:>)|(<:p:>;(!s:>)%(101,1000000)));!r:>' -f fullsuite/RespondBQR/Discrete/1M/RespondBQR100.txt >> /dev/null 2>&1" \
     --command-name RespondBQR1000 \
-        "montre -b '<:!q;((<:p:>;(!s)%(0,299);<:s:>)|(<:p:>;(!s:>)%(1001,1000000)));!r:>' -f /timescales/fullsuite/RespondBQR/Discrete/1M/RespondBQR1000.txt >> /dev/null 2>&1" \
+        "montre -b '<:!q;((<:p:>;(!s)%(0,299);<:s:>)|(<:p:>;(!s:>)%(1001,1000000)));!r:>' -f fullsuite/RespondBQR/Discrete/1M/RespondBQR1000.txt >> /dev/null 2>&1" \
 
 
 
